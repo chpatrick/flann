5d860c9ebaa155df85b94f40a503586e27ee9392
Patrick Chilton <chpatrick@gmail.com>
2021-02-02 13:44:28 +0100

    Enable CUDA in CI.

--- default.nix
+++ default.nix
@@ -1,17 +1,20 @@
-{ stdenv, fetchFromGitHub, fetchpatch, unzip, cmake, python, pkgconfig, gtest, hdf5, lz4 }:
+{ gccStdenv, overrideCC, lib, fetchpatch, unzip, cmake, python, pkgconfig, gtest, hdf5, lz4, cudatoolkit }:
 
-stdenv.mkDerivation {
+(overrideCC gccStdenv cudatoolkit.cc).mkDerivation {
   pname = "flann";
   version = "1.9.1";
 
   src = ./.;
 
   nativeBuildInputs = [ cmake pkgconfig ];
-  buildInputs = [ python gtest hdf5 lz4 ];
+  buildInputs = [ python gtest hdf5 lz4 cudatoolkit ];
+  cmakeFlags = [
+    "-DBUILD_CUDA_LIB=ON"
+  ];
 
   meta = {
     homepage = http://people.cs.ubc.ca/~mariusm/flann/;
-    license = stdenv.lib.licenses.bsd3;
+    license = lib.licenses.bsd3;
     description = "Fast approximate nearest neighbor searches in high dimensional spaces";
   };
 }
